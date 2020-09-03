e96f1846281600c90aefbfa4617ec5b6638aa815
m.norouzi <m.norouzi@hamisystem.ir>
2020-09-04 04:18:35 +0430

    resolve test class context with mockito

--- data/src/androidTest/java/com/home/servicepresentation/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/home/servicepresentation/data/ExampleInstrumentedTest.kt
@@ -2,11 +2,22 @@ package com.home.servicepresentation.data
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.gson.JsonSyntaxException
+import com.home.servicepresentation.data.models.base.BaseModel
+import com.home.servicepresentation.data.models.detail.DetailModel
+import com.home.servicepresentation.data.models.home.HomeModel
+import com.home.servicepresentation.data.network.CheckNetwork
+import com.home.servicepresentation.data.network.Network
+import org.hamcrest.CoreMatchers
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import java.io.BufferedInputStream
+import java.io.FileNotFoundException
+import java.net.MalformedURLException
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +26,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
